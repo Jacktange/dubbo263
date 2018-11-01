e757f33627b48523aa343d73f64a616565a2420b
Jacktange <java@9f0.net>
2018-11-01 13:37:47 +0800

    Add notes for ServiceBean - Service Export.

--- dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/ServiceBean.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/ServiceBean.java
@@ -80,10 +80,10 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
         SpringExtensionFactory.addApplicationContext(applicationContext);
         if (applicationContext != null) {
             SPRING_CONTEXT = applicationContext;
-            try {
+            try {// 为了兼容Spring2.0.1的版本，为了向前兼容spring的事件机制
                 Method method = applicationContext.getClass().getMethod("addApplicationListener", new Class<?>[]{ApplicationListener.class}); // backward compatibility to spring 2.0.1
                 method.invoke(applicationContext, new Object[]{this});
-                supportedApplicationListener = true;
+                supportedApplicationListener = true;// 设置为true，后面 isDelay()判断用到
             } catch (Throwable t) {
                 if (applicationContext instanceof AbstractApplicationContext) {
                     try {
@@ -115,8 +115,10 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
     }
 
     /**
+     *  暴露服务
      *  ApplicationListener<ContextRefreshedEvent>
      *  实现ApplicationListener接口监听ContextRefreshedEvent事件 - 容器加载完成事件
+     *  触发时机：在spring的IOC容器完成所有的bean的实例化和初始化工作之后，会发出一个ContextRefreshedEvent类型的事件
      * @param event
      */
     @Override
@@ -129,34 +131,48 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
         }
     }
 
+    /**
+     *  是否延迟
+     *   通常我们的dubbo服务的配置：<dubbo:service  interface="com.xxx.Yxxx" ref="userService" protocol="dubbo" retries="0" /> ，
+     *  在这样的情况下 isDelay()方法返回true，所以我们的暴露将会发生在下面的方法中
+     * @return
+     */
     private boolean isDelay() {
         Integer delay = getDelay();
         ProviderConfig provider = getProvider();
         if (delay == null && provider != null) {
             delay = provider.getDelay();
         }
-        return supportedApplicationListener && (delay == null || delay == -1);
+        // supportedApplicationListener属性在ServiceBean实现ApplicationContextAware接口的setApplicationContext方法中设置为true的
+        return supportedApplicationListener && (delay == null || delay == -1);// 如果<dubbo:service/>没有配delay属性或将其配置为-1都会延迟发布服务
     }
 
+    /**
+     *  对服务Bean的实例进行初始化
+     * @throws Exception
+     */
     @Override
     @SuppressWarnings({"unchecked", "deprecation"})
     public void afterPropertiesSet() throws Exception {
-        if (getProvider() == null) {
+        if (getProvider() == null) {// 获取provider配置
+            // 当某个<dubbo:service/>没有绑定相应的<dubbo:provider/>的时候
             Map<String, ProviderConfig> providerConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ProviderConfig.class, false, false);
             if (providerConfigMap != null && providerConfigMap.size() > 0) {
+                // 在spring的IOC容器中查找type为ProtocolConfig.class或其子类的bean
                 Map<String, ProtocolConfig> protocolConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ProtocolConfig.class, false, false);
+                // 存在<dubbo:provider/>但不存在<dubbo:protocol/>配置的情况,也就是说旧版本的protocol配置需要从provider中提取
                 if ((protocolConfigMap == null || protocolConfigMap.size() == 0)
-                        && providerConfigMap.size() > 1) { // backward compatibility
+                        && providerConfigMap.size() > 1) { // backward compatibility 兼容旧版本
                     List<ProviderConfig> providerConfigs = new ArrayList<ProviderConfig>();
                     for (ProviderConfig config : providerConfigMap.values()) {
                         if (config.isDefault() != null && config.isDefault().booleanValue()) {
-                            providerConfigs.add(config);
+                            providerConfigs.add(config);// 当<dubbo:provider  default="true"/>时，providerConfigs才会加入
                         }
                     }
-                    if (!providerConfigs.isEmpty()) {
+                    if (!providerConfigs.isEmpty()) {// 在配置provider的同时，也从默认的<dubbo:provider/>中提取protocol的配置
                         setProviders(providerConfigs);
                     }
-                } else {
+                } else {// 已存在<dubbo:protocol/>配置,则找出默认的<dubbo:provider/>配置
                     ProviderConfig providerConfig = null;
                     for (ProviderConfig config : providerConfigMap.values()) {
                         if (config.isDefault() == null || config.isDefault().booleanValue()) {
@@ -166,14 +182,22 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
                             providerConfig = config;
                         }
                     }
+                    /**
+                     * 补充一下什么是默认的<dubbo:provider/>，在dubbo配置文件中，可以有多个<dubbo:provider/>配置，
+                     * 如果某个<dubbo:provide/>配置的default属性为true。这个默认配置只能一个.
+                     */
                     if (providerConfig != null) {
                         setProvider(providerConfig);
                     }
                 }
             }
         }
+        /**
+         *  通过<dubbo:application/>的形式来配置application，也可以直接以bean的形式去配，
+         *  这个bean对应的Class就是ApplicationConfig.class.
+         */
         if (getApplication() == null
-                && (getProvider() == null || getProvider().getApplication() == null)) {
+                && (getProvider() == null || getProvider().getApplication() == null)) {// 获取application配置
             Map<String, ApplicationConfig> applicationConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ApplicationConfig.class, false, false);
             if (applicationConfigMap != null && applicationConfigMap.size() > 0) {
                 ApplicationConfig applicationConfig = null;
@@ -191,7 +215,7 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
             }
         }
         if (getModule() == null
-                && (getProvider() == null || getProvider().getModule() == null)) {
+                && (getProvider() == null || getProvider().getModule() == null)) {// 获取module配置
             Map<String, ModuleConfig> moduleConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ModuleConfig.class, false, false);
             if (moduleConfigMap != null && moduleConfigMap.size() > 0) {
                 ModuleConfig moduleConfig = null;
@@ -210,7 +234,7 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
         }
         if ((getRegistries() == null || getRegistries().isEmpty())
                 && (getProvider() == null || getProvider().getRegistries() == null || getProvider().getRegistries().isEmpty())
-                && (getApplication() == null || getApplication().getRegistries() == null || getApplication().getRegistries().isEmpty())) {
+                && (getApplication() == null || getApplication().getRegistries() == null || getApplication().getRegistries().isEmpty())) {// 获取注册中心的配置
             Map<String, RegistryConfig> registryConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, RegistryConfig.class, false, false);
             if (registryConfigMap != null && registryConfigMap.size() > 0) {
                 List<RegistryConfig> registryConfigs = new ArrayList<RegistryConfig>();
@@ -226,7 +250,7 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
         }
         if (getMonitor() == null
                 && (getProvider() == null || getProvider().getMonitor() == null)
-                && (getApplication() == null || getApplication().getMonitor() == null)) {
+                && (getApplication() == null || getApplication().getMonitor() == null)) {// 获取monitor配置
             Map<String, MonitorConfig> monitorConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, MonitorConfig.class, false, false);
             if (monitorConfigMap != null && monitorConfigMap.size() > 0) {
                 MonitorConfig monitorConfig = null;
@@ -244,7 +268,7 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
             }
         }
         if ((getProtocols() == null || getProtocols().isEmpty())
-                && (getProvider() == null || getProvider().getProtocols() == null || getProvider().getProtocols().isEmpty())) {
+                && (getProvider() == null || getProvider().getProtocols() == null || getProvider().getProtocols().isEmpty())) {// 获取protocol配置
             Map<String, ProtocolConfig> protocolConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ProtocolConfig.class, false, false);
             if (protocolConfigMap != null && protocolConfigMap.size() > 0) {
                 List<ProtocolConfig> protocolConfigs = new ArrayList<ProtocolConfig>();
@@ -258,14 +282,15 @@ public class ServiceBean<T> extends ServiceConfig<T> implements InitializingBean
                 }
             }
         }
+        // 获取<dubbo:service/>的path属性，path即服务的发布路径
         if (getPath() == null || getPath().length() == 0) {
             if (beanName != null && beanName.length() > 0
                     && getInterface() != null && getInterface().length() > 0
                     && beanName.startsWith(getInterface())) {
-                setPath(beanName);
+                setPath(beanName);// 如果没有设置path属性，则默认会以beanName作为path
             }
         }
-        if (!isDelay()) {
+        if (!isDelay()) {// 是否延迟暴露
             export();
         }
     }
